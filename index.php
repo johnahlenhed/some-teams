9ab94ec0796538ea6d6789f2e04aa502833ea801
John Ahlenhed <johnahlenhed@gmail.com>
2025-10-09 20:07:18 +0200

    Body: Main functions and style

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 <?php
+require __DIR__. '/data.php';
 require __DIR__. '/includes/header.php';
 ?>
 
 <main>
     <h1>Start Page</h1>
+
+    <section class="teams-section">
+        <?php foreach ($teams as $teamName => $teamData): ?>
+            <div class="teams-section-div">
+            <a href="<?php echo $teamData['url'] ?>">
+            <?php echo $teamName?>
+            <img src="<?php echo $teamData['logo'] ?>" alt="Team Logo">
+            </a>
+            </div>
+        <?php endforeach ?>
+    </section>
+
 </main>
+
+
 
 <?php
 require __DIR__. '/includes/footer.php';
