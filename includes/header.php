691981ac9a9e2fbbc79527d4f5848e5969d4784b
John Ahlenhed <johnahlenhed@gmail.com>
2025-10-09 16:07:57 +0200

    Feature: add navbar with home and about links

--- includes/header.php
+++ includes/header.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="../assets/style.css">
 </head>
 <body>
     <header>
         <nav>
-            <a href="index.php">Home</a>
-            <a href="about.php">About</a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+            </ul>
         </nav>
     </header>
 </body>
